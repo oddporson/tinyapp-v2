1b38a159a4aaa290d0e091615509425ed848f23c
Porson <porsonlee@gmail.com>
2020-03-07 01:16:10 -0800

    moved PORT listen to bottom of the codes

--- express_server.js
+++ express_server.js
@@ -12,9 +12,32 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/urls.json", (req,res) => {
+app.get("/urls.json", (req ,res) => {
   res.send(urlDatabase);
-})
+});
+
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, ()=> {
   console.log(`Example app listening on port ${PORT}!`);
