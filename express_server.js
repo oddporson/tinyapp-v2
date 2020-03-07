a9335af7a6d7f42f02a45012bed15926c87b31dd
Porson <porsonlee@gmail.com>
2020-03-07 01:25:29 -0800

    created new route for urls so we render the urls_index

--- express_server.js
+++ express_server.js
@@ -15,13 +15,9 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/urls.json", (req ,res) => {
-  res.send(urlDatabase);
-});
-
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase }
+  res.render("urls_index", templateVars);
 });
 
 
